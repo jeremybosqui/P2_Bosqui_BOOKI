0041c079fb2f1b937e26b72b11e604136082c8c8
jeremybosqui <bosdani97@gmail.com>
2021-10-24 16:55:35 +0200

    first

---
+++ asset/js/main.js
@@ -0,0 +1,24 @@
+let scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+let rootElement = document.documentElement;
+
+function handleScroll() {
+  // donner les indications au bouton en lui disant de remonter en haut de la page et definir un height 
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.45) {
+    // montrer le bouton 
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // cacher le bouton grave au remove 
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+function scrollToTop() {
+  // Scroll en haut de la page avec un comportement doux pour que le scroll ne soit pas violent vers le haut de la page
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
